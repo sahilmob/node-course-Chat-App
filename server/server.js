c502ca184918d6516d3efee0c07243c39a313f5b
root <root@DESKTOP-COMGLI5.localdomain>
2018-06-20 07:32:17 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const publicPAth = path.join(__dirname, '../public/')
+const port = process.env.PORT || 3000
+var app = express()
+
+app.use(express.static(publicPAth))
+
+
+app.listen(port, () => {
+    console.log(`Started at port ${port}`)
+})
